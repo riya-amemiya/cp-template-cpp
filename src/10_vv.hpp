ab4ee9fe30e1948097afa36830741be06a25804d
Riya Amemiya <riya-amemiya+github@tokidux.com>
2026-09-25 11:41:31 +0900

    fix

--- src/10_vv.hpp
+++
@@ -1,4 +0,0 @@
-// 2次元ベクトルの初期化
-template <typename T> vector<vector<T>> vv(int n, int m, T x = T()) {
-  return vector<vector<T>>(n, vector<T>(m, x));
-}
--- src/99_main.hpp
+++
@@ -1,8 +0,0 @@
-// メイン関数
-#ifndef TESTING
-int main() {
-  fastio();
-
-  return 0;
-}
-#endif
